5e202da4640a9e4e2479e0cf6daeaf0c0e4c6058
kortchor <roblexdonfack@gmail.com>
2026-07-08 12:40:54 +0200

    Modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "./context/AuthContext";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
